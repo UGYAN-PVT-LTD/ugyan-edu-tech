33e40a7bb5209a76387622029a92d71954710adb
Murali Krishna Korrapati <muralikrishnakorrapati6@gmail.com>
2025-07-13 00:19:19 +0530

    Scroll to top uncommented and tab logo added

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,16 +1,15 @@
 // ScrollToTop.jsx
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
-  // useEffect(() => {
-  //   // Scroll on route change or reload
-  //   if (typeof window !== "undefined") {
-  //     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  //   }
-  // }, [pathname]);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    }
+  }, [pathname]);
 
   return null;
 };
