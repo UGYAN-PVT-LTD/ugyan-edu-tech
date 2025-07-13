6d0a975f53b4293d9ce31bc964ffe2b05903b787
vijay120922 <n200305@rguktn.ac.in>
2025-07-13 20:12:17 +0530

    About us section updated

--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import './AboutUsSection.css';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import React, { useEffect, useState } from "react";
+import "./AboutUsSection.css";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const AboutUsSection = () => {
   const [loading, setLoading] = useState(true);
@@ -15,7 +15,11 @@ const AboutUsSection = () => {
     <section className="why-choose-us mt-5">
       <h2>Who are We ?</h2>
       <p className="subheading">
-        {loading ? <Skeleton width={500} height={20} /> : `Unlock your true potential and discover a world of opportunities that align with your skills, interests, and aspirations`}
+        {loading ? (
+          <Skeleton width={500} height={20} />
+        ) : (
+          `Unlock your true potential and discover a world of opportunities that align with your skills, interests, and aspirations`
+        )}
       </p>
 
       <div className="choose-grid">
@@ -31,18 +35,27 @@ const AboutUsSection = () => {
           <div className="text-box shadow-box">
             {loading ? (
               <div>
-                <Skeleton height={20} width={200} style={{ marginBottom: 10 }} />
+                <Skeleton
+                  height={20}
+                  width={200}
+                  style={{ marginBottom: 10 }}
+                />
                 <Skeleton count={4} />
               </div>
             ) : (
-              <p>
-                <strong style={{ fontSize: '16px' }}>Leela Krishna Vaka</strong><br />
-                <strong style={{ fontSize: '10px' }}>Co - founder & Managing director</strong><br />
-                <br />
-               
-
-
-Education has the power to transform lives. At Ugyan, we're committed to making learning accessible and engaging for all students. Join us in this journey to redefine education."</p>
+              <div>
+                <div className="flex flex-col mb-3">
+                  <span className="text-lg font-bold">Leela Krishna Vaka</span>
+                  <span className="text-sm text-gray-600">
+                    Co-founder & Managing director
+                  </span>
+                </div>
+                <div>
+                  Education has the power to transform lives. At Ugyan, we're
+                  committed to making learning accessible and engaging for all
+                  students. Join us in this journey to redefine education."
+                </div>
+              </div>
             )}
           </div>
         </div>
@@ -53,19 +66,30 @@ Education has the power to transform lives. At Ugyan, we're committed to making 
             <div className="text-box shadow-box">
               {loading ? (
                 <div>
-                  <Skeleton height={20} width={200} style={{ marginBottom: 10 }} />
+                  <Skeleton
+                    height={20}
+                    width={200}
+                    style={{ marginBottom: 10 }}
+                  />
                   <Skeleton count={4} />
                 </div>
               ) : (
-              <p>
-                <strong style={{ fontSize: '16px' }}>Aswini Thakkellapati</strong><br />
-                <strong style={{ fontSize: '10px' }}>Co-founder & CEO
-</strong><br />
-                <br />
-               
-
-
-I believe that education is the cornerstone of a brighter future. At Ugyan, we are driven by the mission to innovate and enhance learning through cutting-edge technology. Our team is dedicated to creating solutions that inspire curiosity, foster creativity, and empower individuals to reach their full potential.</p> )}
+                <div>
+                  <div className="flex flex-col mb-3">
+                    <span className="text-lg font-bold">
+                      Aswini Thakkellapati
+                    </span>
+                    <span className="text-sm text-gray-600">
+                      Co-founder & CEO
+                    </span>
+                  </div>
+                  I believe that education is the cornerstone of a brighter
+                  future. At Ugyan, we are driven by the mission to innovate and
+                  enhance learning through cutting-edge technology. Our team is
+                  dedicated to creating solutions that inspire curiosity and
+                  empower individuals to reach their full potential.
+                </div>
+              )}
             </div>
           </div>
           <div className="image-box peach-bg">
