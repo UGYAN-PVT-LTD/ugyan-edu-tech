a816b3b0ca91328fec3e3d2b85c9a0d2a0fff94c
UgyanEduTech <backend@ugyan.in>
2025-11-20 20:51:53 +0530

    Update CircularGallery.jsx

--- src/components/CircularGallery.jsx
+++ src/components/CircularGallery.jsx
@@ -222,6 +222,8 @@ const defaultItems = [
   { image: "5.jpg" },
   { image: "6.jpg" },
   { image: "7.jpg" },
+  { image: "8.jpg" },
+  { image: "9.jpg" },
   { image: "1.jpg" },
   { image: "2.jpg" },
   { image: "3.jpg" },
